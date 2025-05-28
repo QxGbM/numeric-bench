28b945cdad1e79beaf136f8b6d1a5445154518e7
Qianxiang Ma <ma@rio.scrc.iir.isct.ac.jp>
2025-05-28 09:44:12 +0900

    fixed point mul

---
+++ fl-div.cpp
@@ -0,0 +1,105 @@
+
+#include <commons.hpp>
+
+uint64_t fixed_point_mul_i15x4(const uint64_t* a, const uint64_t* b) {
+  uint64_t ia0 = a[0];
+  int32_t a0 = int16_t(ia0);
+  int32_t a1 = int16_t(ia0 >> 16);
+  int32_t a2 = int16_t(ia0 >> 32);
+  int32_t a3 = int16_t(ia0 >> 48);
+
+  uint64_t ib0 = b[0];
+  int32_t b0 = int16_t(ib0);
+  int32_t b1 = int16_t(ib0 >> 16);
+  int32_t b2 = int16_t(ib0 >> 32);
+  int32_t b3 = int16_t(ib0 >> 48);
+
+  int32_t m0 = a0 * b0;
+  int32_t m1 = a1 * b0 + a0 * b1;
+  int32_t m2 = a2 * b0 + a1 * b1 + a0 * b2;
+  int32_t m3 = a3 * b0 + a2 * b1 + a1 * b2 + a0 * b3;
+  int32_t m4 = a3 * b1 + a2 * b2 + a1 * b3;
+
+  a0 = m0 & 0x3FFF;
+  a1 = m1 & 0x3FFF;
+  a2 = m2 & 0x3FFF;
+  a3 = m3 & 0x3FFF;
+
+  m0 = m0 >> 14;
+  m1 = (m1 >> 14) + a0;
+  m2 = (m2 >> 14) + a1;
+  m3 = (m3 >> 14) + a2;
+  m4 = (m4 >> 14) + a3;
+
+  a0 = m1 >> 14;
+  a1 = m2 >> 14;
+  a2 = m3 >> 14;
+  a3 = m4 >> 14;
+
+  m0 = m0 + a0;
+  m1 = (m1 & 0x3FFF) + a1;
+  m2 = (m2 & 0x3FFF) + a2;
+  m3 = (m3 & 0x3FFF) + a3;
+
+  uint32_t ci0 = (m1 << 16) | (m0 & 0xFFFF);
+  uint32_t ci1 = (m3 << 16) | (m2 & 0xFFFF);
+  uint64_t c = (uint64_t(ci1) << 32) | uint64_t(ci0);
+  return c;
+}
+
+int32_t main() {
+  mkl_verbose(0);
+  
+  double b = 1531.2345678913354632132 / 1024;
+  double c = 1.;
+  for (int32_t i = 0; i < 6; ++i) {
+    c = 0.5 * c * (3 - b * c * c);
+  }
+  double d = b * c * c;
+  printf("%e\n", std::abs(d - 1.));
+
+  b = -b;
+  double b_iter = b;
+  int16_t bp[4];
+  for (int32_t i = 0; i < 4; ++i) {
+    b_iter *= (1 << 14);
+    bp[i] = int16_t(b_iter);
+    b_iter -= bp[i];
+  }
+
+  b_iter = 1. / (1 << 14);
+  double reb = 0;
+  for (int32_t i = 0; i < 4; ++i) {
+    reb += b_iter * bp[i];
+    b_iter *= 1. / (1 << 14);
+  }
+
+  printf("%e\n", std::abs(reb - b) / b);
+  printf("%d %d %d %d\n", bp[0], bp[1], bp[2], bp[3]);
+
+  double c_iter = c * 0.5;
+  int16_t cp[4];
+  for (int32_t i = 0; i < 4; ++i) {
+    c_iter *= (1 << 14);
+    cp[i] = int16_t(c_iter);
+    c_iter -= cp[i];
+  }
+
+  int16_t ap[4];
+  uint64_t a = fixed_point_mul_i15x4((uint64_t*)bp, (uint64_t*)cp);
+  ap[0] = int16_t(a);
+  ap[1] = int16_t(a >> 16);
+  ap[2] = int16_t(a >> 32);
+  ap[3] = int16_t(a >> 48);
+
+  b_iter = 2. / (1 << 14);
+  double re_m = 0;
+  for (int32_t i = 0; i < 4; ++i) {
+    re_m += b_iter * ap[i];
+    b_iter *= 1. / (1 << 14);
+  }
+
+  printf("%.12e\n", std::abs(re_m - b * c));
+
+  return 0;
+}
--- iter_lra.cpp
+++ iter_lra.cpp
@@ -4,12 +4,12 @@
 int32_t main() {
   mkl_verbose(0);
 
-  int32_t M = 200, N = 50;
+  int32_t M = 800, N = 100;
   Eigen::MatrixXd matA(M, N);
   random_vector(M * N, (double*)matA.data());
 
   Eigen::MatrixXd AAT = matA.adjoint() * matA;
-  for (int32_t i = 0; i < 5; ++i) {
+  for (int32_t i = 0; i < 6; ++i) {
     AAT /= AAT.norm();
     AAT = AAT.adjoint() * AAT;
   }
@@ -18,34 +18,49 @@ int32_t main() {
   Eigen::MatrixXd ref = matA;
 
   Eigen::ColPivHouseholderQR<Eigen::MatrixXd> cpqr(matA);
-  Eigen::MatrixXd qr = cpqr.matrixQR();
+  Eigen::MatrixXd matR = cpqr.matrixQR();
   cpqr.setThreshold(1.e-12);
 
-  printf("rank cpqr: %d (%e, %e)\n", cpqr.rank(), qr(cpqr.rank() - 1, cpqr.rank() - 1), qr(0, 0));
-  
-  int32_t rank_tot = 0;
-  for (int32_t i = 0; i < 10; ++i) {
-    double nrm = matA.lpNorm<Eigen::Infinity>();
-    Eigen::MatrixXd matB = matA * (127. / nrm);
+  printf("rank cpqr: %d (%e, %e)\n", (int32_t)cpqr.rank(), matR(cpqr.rank() - 1, cpqr.rank() - 1), matR(0, 0));
+
+  int32_t iters = 10, rank_tot = 0;
+  Eigen::MatrixXd Uf(M, iters * N);
+  Eigen::MatrixXd Vf(N, iters * N);
+  for (int32_t i = 0; i < iters; ++i) {
+    Eigen::MatrixXd matB = matA;
+    double nrm = 127. / matB.lpNorm<Eigen::Infinity>();
 
     Eigen::MatrixXf matAi(M, N);
-    std::transform(matB.data(), matB.data() + M * N, matAi.data(), [](double e) { return float(e); });
+    std::transform(matB.data(), matB.data() + M * N, matAi.data(), [=](double e) { return float(nrm * e); });
 
-    Eigen::JacobiSVD<Eigen::MatrixXf> svd(matAi.transpose() * matAi, Eigen::ComputeThinU); // fp32
+    Eigen::JacobiSVD<Eigen::MatrixXf> svd(matAi.transpose() * matAi, Eigen::ComputeThinU);
     svd.setThreshold(1.e-6);
 
     int32_t rank = svd.rank();
     Eigen::MatrixXf V = svd.matrixU().leftCols(rank);
-    Eigen::MatrixXf U = matAi * V;
+    Eigen::MatrixXf U = (1. / nrm) * matAi * V;
 
     Eigen::MatrixXd Vd(N, rank), Ud(M, rank);
     std::transform(V.data(), V.data() + N * rank, Vd.data(), [](float e) { return double(e); });
     std::transform(U.data(), U.data() + M * rank, Ud.data(), [](float e) { return double(e); });
 
-    Eigen::MatrixXd appx = Ud * Vd.transpose();
-    matA -= (nrm / 127.) * appx;
+    Vf.middleCols(rank_tot, rank) = Vd;
+    Uf.middleCols(rank_tot, rank) = Ud;
 
-    printf("%d %e %e %d\n", i, (appx - matB).norm() / matB.norm(), matA.norm(), rank_tot += rank);
+    Eigen::MatrixXd appx = Uf.middleCols(rank_tot, rank) * Vf.middleCols(rank_tot, rank).transpose();
+    matA -= appx;
+    rank_tot += rank;
+
+    printf("%d %e %e %d\n", i, (appx - matB).norm() / matB.norm(), matA.norm() / ref.norm(), rank_tot);
   }
+
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(Vf.leftCols(rank_tot));
+  matR = qr.matrixQR().triangularView<Eigen::Upper>();
+
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(Uf.leftCols(rank_tot) * matR.transpose());
+  svd.setThreshold(1.e-12);
+  int32_t rank_svd = svd.rank();
+  printf("rank svd: %d, %e\n", rank_svd, (Uf * Vf.transpose() - ref).norm() / ref.norm());
+
   return 0;
 }
