a70f777c521213d412385820165ce62f8df8705f
Qianxiang Ma <qianxiang.ma@case.edu>
2025-07-15 15:45:28 +0900

    updates for test

--- h_matmul.cpp
+++ h_matmul.cpp
@@ -20,14 +20,14 @@ int32_t main() {
   cublasCreate(&handle);
   cublasSetStream(handle, stream);
 
-  const int32_t m = 8000, k = 64;
+  const int32_t m = 3000, k = 256;
 
   std::mt19937_64 gen;
   std::normal_distribution<double> dist(0., 1.);
 
   std::complex<double>* d_A, * d_C;
-  cudaMallocManaged(reinterpret_cast<void**>(&d_A), m * m * sizeof(std::complex<double>), cudaMemAttachGlobal);
-  cudaMallocManaged(reinterpret_cast<void**>(&d_C), m * m * sizeof(std::complex<double>), cudaMemAttachGlobal);
+  cudaMallocManaged(reinterpret_cast<void**>(&d_A), 2 * m * m * sizeof(std::complex<double>), cudaMemAttachGlobal);
+  cudaMallocManaged(reinterpret_cast<void**>(&d_C), 2 * m * m * sizeof(std::complex<double>), cudaMemAttachGlobal);
   cudaMemset(d_C, 0, m * m * sizeof(std::complex<double>));
 
   for (int32_t i = 0; i < m * m; ++i)
@@ -48,17 +48,66 @@ int32_t main() {
 
   start = omp_get_wtime();
   for (int32_t i = 0; i < loops; ++i)
-    internal::Cholesky::minus_AHA_gemmk64_double_complex(stream, m, d_A, d_C, m);
+    internal::Cholesky::minus_AHA_gemmk_double_complex(stream, m, d_A, d_C, m);
   cudaDeviceSynchronize();
   lapse = omp_get_wtime() - start;
 
   printf("<zgemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
-  fprintf(stderr, "%s\n", cudaGetErrorString(cudaGetLastError()));
 
   double nrm;
   cublasDznrm2(handle, m * m, (const cuDoubleComplex*)d_C, 1, &nrm);
   printf("%le\n", nrm / m / m);
 
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    cublasCgemm(handle, CUBLAS_OP_C, CUBLAS_OP_N, m, m, k, (const cuComplex*)&one, (const cuComplex*)d_A, m, (const cuComplex*)d_A, m, (const cuComplex*)&one, (cuComplex*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<cublas cgemm> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    internal::Cholesky::minus_AHA_gemmk_float_complex(stream, m, (std::complex<float>*)d_A, (std::complex<float>*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<cgemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    internal::Cholesky::minus_AHA_gemmk_double2_complex(stream, m, (complex_double2*)d_A, (complex_double2*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<cdd_gemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    internal::Cholesky::minus_AHA_gemmk_float4_complex(stream, m, (complex_float4*)d_A, (complex_float4*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<cqf_gemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    internal::Cholesky::minus_ATA_gemmk_double2(stream, m, (double2*)d_A, (double2*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<dd_gemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+  start = omp_get_wtime();
+  for (int32_t i = 0; i < loops; ++i)
+    internal::Cholesky::minus_ATA_gemmk_float4(stream, m, (float4*)d_A, (float4*)d_C, m);
+  cudaDeviceSynchronize();
+  lapse = omp_get_wtime() - start;
+
+  printf("<qf_gemmk> time: %f ms. GFLOPS: %f\n", lapse * 1000 / loops, gflops / lapse);
+
+
+  fprintf(stderr, "%s\n", cudaGetErrorString(cudaGetLastError()));
   cudaFree(d_A);
   cudaFree(d_C);
 
